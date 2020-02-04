4719af0c500c69c1efb7eeb3d0eb7deb2e6b86ee
Franz <shedali@gmail.com>
2020-02-04 16:05:04 +0000

    eslint

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,60 +1,60 @@
-const fs = require('fs');
-const path = require('path');
-const toVFile = require('to-vfile');
-const { render, renderFromFile, getDestinationDir } = require('../src/utils');
+const fs = require('fs')
+const path = require('path')
+const toVFile = require('to-vfile')
+const { render, renderFromFile, getDestinationDir } = require('../src/utils')
 
-const fixturesDir = path.join(__dirname, '/fixtures');
-const runtimeDir = path.join(__dirname, '/runtime');
+const fixturesDir = path.join(__dirname, '/fixtures')
+const runtimeDir = path.join(__dirname, '/runtime')
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000
 
 // Utility function to add metdata to a vFile.
-function addMetadata(vFile, destinationFilePath) {
+function addMetadata (vFile, destinationFilePath) {
   vFile.data = {
     destinationFilePath,
-    destinationDir: path.dirname(destinationFilePath),
-  };
+    destinationDir: path.dirname(destinationFilePath)
+  }
 }
 
 describe('remark-mermaid utils', () => {
   it('renders a mermaid graph', () => {
-    const mermaidExample = fs.readFileSync(`${fixturesDir}/example.mmd`, 'utf8');
-    let renderedGraphFile;
+    const mermaidExample = fs.readFileSync(`${fixturesDir}/example.mmd`, 'utf8')
+    let renderedGraphFile
 
     try {
-      renderedGraphFile = render(mermaidExample, runtimeDir);
+      renderedGraphFile = render(mermaidExample, runtimeDir)
     } catch (err) {
-      console.error(err.message);
+      console.error(err.message)
     }
 
-    expect(renderedGraphFile).not.toBeUndefined();
-  });
+    expect(renderedGraphFile).not.toBeUndefined()
+  })
 
   it('renders from a file a mermaid graph', () => {
-    let renderedGraphFile;
+    let renderedGraphFile
 
     try {
-      renderedGraphFile = renderFromFile(`${fixturesDir}/example.mmd`, runtimeDir);
+      renderedGraphFile = renderFromFile(`${fixturesDir}/example.mmd`, runtimeDir)
     } catch (err) {
-      console.error(err.message);
+      console.error(err.message)
     }
 
-    expect(renderedGraphFile).not.toBeUndefined();
-  });
+    expect(renderedGraphFile).not.toBeUndefined()
+  })
 
   it('handles explicity set destination', () => {
-    const srcFile = `${fixturesDir}/code-block.md`;
-    const destFile = `${runtimeDir}/code-block.md`;
-    const vfile = toVFile.readSync(srcFile);
-    addMetadata(vfile, destFile);
+    const srcFile = `${fixturesDir}/code-block.md`
+    const destFile = `${runtimeDir}/code-block.md`
+    const vfile = toVFile.readSync(srcFile)
+    addMetadata(vfile, destFile)
 
-    expect(getDestinationDir(vfile)).toEqual(runtimeDir);
-  });
+    expect(getDestinationDir(vfile)).toEqual(runtimeDir)
+  })
 
   it('handles fallback destination', () => {
-    const srcFile = `${fixturesDir}/code-block.md`;
-    const vfile = toVFile.readSync(srcFile);
+    const srcFile = `${fixturesDir}/code-block.md`
+    const vfile = toVFile.readSync(srcFile)
 
-    expect(getDestinationDir(vfile)).toEqual(fixturesDir);
-  });
-});
+    expect(getDestinationDir(vfile)).toEqual(fixturesDir)
+  })
+})
